94e5bb7d77f9d9555e7322cf82b5ab01fe6318ec
Unknown <suvambasak2008@gmail.com>
2018-04-19 11:30:01 +0530

    notification

--- notification.php
+++ notification.php
@@ -2,22 +2,6 @@
 	include('dbconnect.php');
 	$response = array();
 
-	function isUserExist($con,$androidId,$username){
-		$sql = "SELECT * FROM `device` WHERE username = '".$username."' AND androidId = '".$androidId."'";
-		$result = mysqli_query($con,$sql);
-		$valueOwner = @mysqli_num_rows($result);
-
-		$sql = "SELECT * FROM `memberDetails` WHERE androidId = '".$androidId."' AND username = '".$username."'";
-		$result = mysqli_query($con,$sql);
-		$valueMember = @mysqli_num_rows($result);
-
-
-		if ($valueOwner or $valueMember)
-			return true;
-		return false;
-	}
-
-
 
 
 	if($_SERVER['REQUEST_METHOD'] == 'POST'){
@@ -26,35 +10,35 @@
 			isset($_POST['email']) and
 			isset($_POST['androidId'])
 		){
-			if (isUserExist($con,$_POST['androidId'],$_POST['username'])){
-				$sql = "SELECT * FROM `notification` WHERE username = '".$_POST['username']."' AND email = '".$_POST['email']."'";
-				$result = mysqli_query($con,$sql);
-				$value = @mysqli_num_rows($result);
+			
+			$sql = "SELECT notification.id, notification.username, notification.email, notification.notifyText, notification.imageId FROM `notification`, `access`, `owner` WHERE owner.email = '".$_POST['email']."' AND owner.android_id = '".$_POST['androidId']."' AND access.username = '".$_POST['username']."' AND access.username = notification.username AND access.email = owner.email AND owner.email = notification.email";
+			$result = mysqli_query($con,$sql);
+			$value = @mysqli_num_rows($result);
 
 
-				if($value){
+			if($value){
 					//fetching info.
-					$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+				$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
 					//creating reply.
-					$response['error'] = false;
-					$response['message'] = "Found";
-					$response['notifyText'] = $row['notifyText'];
-					$response['imageId'] = $row['imageId'];
+				$response['error'] = false;
+				$response['message'] = "Found";
+				$response['notifyText'] = $row['notifyText'];
+				$response['imageId'] = $row['imageId'];
 
-					$id = $row['id'];
+				$id = $row['id'];
 
 
 
-					$sql = "DELETE FROM `notification` WHERE id = '$id'";
-					$result = mysqli_query($con,$sql);
+				$sql = "DELETE FROM `notification` WHERE id = '$id'";
+				$result = mysqli_query($con,$sql);
 
-				}else{
-					//reply when username and password is not valid.
-					$response['error'] = true;
-					$response['message'] = "NotFound";
-				}
+			}else{
+				//reply when username and password is not valid.
+				$response['error'] = true;
+				$response['message'] = "NotFound";
 			}
+		
 		}else{
 			$response['error'] = true;
 			$response['message'] = "username and android ID not found";
